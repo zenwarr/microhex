cd19f766a970f4a9eae61abc65cd5d613b62d2d9
zenwarr <zenw@yandex.ru>
2013-08-27 01:06:52 +0600

    add shebang to microhex.py; check interpreter version

--- src/microhex.py
+++ src/microhex.py
@@ -1,9 +1,16 @@
-import os
+#!/usr/bin/env python3
+
 import sys
-import hex.main
-import hex.utils as utils
 
 if __name__ == '__main__':
+    if sys.version_info.major < 3 or sys.version_info.minor < 2:
+        print('Sorry, but your version of Python interpreter is not supported.')
+        sys.exit(-1)
+
+    import os
+    import hex.main
+    import hex.utils as utils
+
     if hasattr(sys, 'frozen'):
         utils.applicationPath = os.path.dirname(sys.executable)
     else:
